9529f0a5ae91180d5fb4c6bb11faf9c8018aaa20
kimkwanka <kimkwanka@gmail.com>
2021-06-29 09:33:48 +0200

    feat: make movie endpoints case insensitive

--- components/movies/moviesService.js
+++ components/movies/moviesService.js
@@ -6,7 +6,7 @@ const Movies = require('./moviesModel');
 const findAllMovies = async () => Movies.find({});
 
 const findMovieByTitle = async (movieTitleToFind) => {
-  const movieToFindByTitle = await Movies.findOne({ Title: movieTitleToFind });
+  const movieToFindByTitle = await Movies.findOne({ Title: new RegExp(`^${movieTitleToFind}$`, 'i') });
 
   if (!movieToFindByTitle) {
     return { statusCode: 404, body: `Couldn't find a movie with title: "${movieTitleToFind}"` };
@@ -15,7 +15,7 @@ const findMovieByTitle = async (movieTitleToFind) => {
 };
 
 const findGenreByName = async (genreNameToFind) => {
-  const movieWithGenre = await Movies.findOne({ 'Genre.Name': genreNameToFind });
+  const movieWithGenre = await Movies.findOne({ 'Genre.Name': new RegExp(`^${genreNameToFind}$`, 'i') });
 
   if (!movieWithGenre) {
     return { statusCode: 404, body: `Couldn't find a genre with name: "${genreNameToFind}"` };
@@ -24,7 +24,7 @@ const findGenreByName = async (genreNameToFind) => {
 };
 
 const findDirectorByName = async (directorNameToFind) => {
-  const movieWithDirector = await Movies.findOne({ 'Director.Name': directorNameToFind });
+  const movieWithDirector = await Movies.findOne({ 'Director.Name': new RegExp(`^${directorNameToFind}$`, 'i') });
 
   if (!movieWithDirector) {
     return { statusCode: 404, body: `Couldn't find a director with name: "${directorNameToFind}"` };
