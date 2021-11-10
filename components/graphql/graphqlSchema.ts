80f1ca1c0cb1cce708f950b867b0280e0dc304b4
kimkwanka <kimkwanka@gmail.com>
2021-11-10 11:28:31 +0100

    feat: use TMDB data for GraphQL queries

--- components/graphql/graphqlSchema.ts
+++ components/graphql/graphqlSchema.ts
@@ -1,32 +1,106 @@
 import { buildSchema } from 'graphql';
 
 const schema = buildSchema(`
-type Genre {
-  name: String!
-  description: String!
-}
+  type Genre {
+    name: String!
+    description: String!
+  }
 
-type Director {
-  name: String!
-  bio: String!
-  birth: String!
-  death: String!
-}
+  type Director {
+    name: String!
+    bio: String!
+    birth: String!
+    death: String!
+  }
 
-type Movie {
-  _id: String!
-  title: String!
-  description: String!
-  genre: Genre!
-  director: Director!
-  slug: String!
-  featured: Boolean!
-  rating: Float!
-}
+  type Movie {
+    _id: String!
+    title: String!
+    description: String!
+    genre: Genre!
+    director: Director!
+    slug: String!
+    featured: Boolean!
+    rating: Float!
+  }
 
-type Query {
-    movies: [Movie]
-}
+  type TMDB_Configuration {
+    base_url: String!
+    secure_base_url: String!
+    backdrop_sizes: [String!]!
+    logo_sizes: [String!]!
+    poster_sizes: [String!]!
+    profile_sizes: [String!]!
+    still_sizes: [String!]!
+  }
+
+  type TMDB_Movie_Simple {
+    adult: Boolean!
+    backdrop_path: String!
+    genre_ids: [String!]!
+    id: Int!
+    original_language: Boolean!
+    original_title: String!
+    overview: String!
+    popularity: Float!
+    poster_path: String!
+    release_date: String!
+    title: String!
+    video: Boolean!
+    vote_average: Float!
+    vote_count: Int!
+  }
+
+  type TMDB_Production_Company {
+    name: String!
+    id: Int!
+    logo_path: String
+    origin_country: String!
+  }
+
+  type TMDB_Production_Country {
+    iso_3166_1: String!
+    name: String!
+  }
+
+  type TMDB_Production_Spoken_Languaeges {
+    iso_639_1: String!
+    name: String!
+  }
+
+  type TMDB_Movie_Detailed {
+    adult: Boolean!
+    backdrop_path: String
+    budget: Int!
+    genre_ids: [String!]!
+    homepage: String
+    id: Int!
+    imdb_id: String
+    original_language: Boolean!
+    original_title: String!
+    overview: String
+    popularity: Float!
+    poster_path: String!
+    production_companies: [TMDB_Production_Company!]!
+    production_countries: [TMDB_Production_Country!]!
+    release_date: String!
+    revenue: Int!
+    runtime: Int
+    spoken_languages: [TMDB_Production_Spoken_Languaeges!]!
+    status: String!
+    tagline: String
+    title: String!
+    video: Boolean!
+    vote_average: Float!
+    vote_count: Int!
+  }
+
+  type Query {
+      movies: [Movie]
+      configuration: TMDB_Configuration
+      discover: [TMDB_Movie_Simple]
+      movie(id: Int!): TMDB_Movie_Detailed
+  }
 `);
 
 export { schema };
--- components/graphql/graphqlService.ts
+++ components/graphql/graphqlService.ts
@@ -1,10 +1,25 @@
+import fetch from 'node-fetch';
+
 import moviesService from '../movies/moviesService';
+
+const TMDB_BASE_API_URL = 'https://api.themoviedb.org/3';
+
+const authorizedFetch = async (apiEndpoint: string) => {
+  const response = await fetch(`${TMDB_BASE_API_URL}${apiEndpoint}`, {
+    headers: { Authorization: `Bearer ${process.env.TMDB_API_TOKEN}` },
+  });
+  const data = await response.json();
+  return data;
+};
 
 const rootValue = {
   movies: async () => {
     const movies = await moviesService.findAllMovies();
     return movies;
   },
+  configuration: async () => (await authorizedFetch('/configuration')).images,
+  discover: async () => (await authorizedFetch('/discover/movie')).results,
+  movie: ({ id }: { id: number }) => authorizedFetch(`/movie/${id}`),
 };
 
 export { rootValue };
