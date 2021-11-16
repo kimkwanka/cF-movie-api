fb444e3bb8c935d06512fcb487995b477225d1a2
kimkwanka <kimkwanka@gmail.com>
2021-11-16 11:03:12 +0100

    fix: remove superfluous operations.graphql

--- domains/graphql/types.d.ts
+++ domains/graphql/types.d.ts
@@ -1,19 +1,10 @@
 import { GraphQLResolveInfo } from 'graphql';
-
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K];
-};
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]?: Maybe<T[SubKey]>;
-};
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]: Maybe<T[SubKey]>;
-};
-export type RequireFields<T, K extends keyof T> = {
-  [X in Exclude<keyof T, K>]?: T[X];
-} & { [P in K]-?: NonNullable<T[P]> };
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type RequireFields<T, K extends keyof T> = { [X in Exclude<keyof T, K>]?: T[X] } & { [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -56,6 +47,7 @@ export type Query = {
   movies?: Maybe<Array<Maybe<Movie>>>;
   users?: Maybe<Array<Maybe<User>>>;
 };
+
 
 export type QueryMovieArgs = {
   id: Scalars['Int'];
@@ -148,55 +140,40 @@ export type User = {
   username: Scalars['String'];
 };
 
+
+
 export type ResolverTypeWrapper<T> = Promise<T> | T;
+
 
 export type ResolverWithResolve<TResult, TParent, TContext, TArgs> = {
   resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
 };
-export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
-  | ResolverFn<TResult, TParent, TContext, TArgs>
-  | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = ResolverFn<TResult, TParent, TContext, TArgs> | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
 
 export type ResolverFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
   args: TArgs,
   context: TContext,
-  info: GraphQLResolveInfo,
+  info: GraphQLResolveInfo
 ) => Promise<TResult> | TResult;
 
 export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
   args: TArgs,
   context: TContext,
-  info: GraphQLResolveInfo,
+  info: GraphQLResolveInfo
 ) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>;
 
 export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
   args: TArgs,
   context: TContext,
-  info: GraphQLResolveInfo,
+  info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
 
-export interface SubscriptionSubscriberObject<
-  TResult,
-  TKey extends string,
-  TParent,
-  TContext,
-  TArgs,
-> {
-  subscribe: SubscriptionSubscribeFn<
-    { [key in TKey]: TResult },
-    TParent,
-    TContext,
-    TArgs
-  >;
-  resolve?: SubscriptionResolveFn<
-    TResult,
-    { [key in TKey]: TResult },
-    TContext,
-    TArgs
-  >;
+export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
 }
 
 export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
@@ -204,53 +181,30 @@ export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
   resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
 }
 
-export type SubscriptionObject<
-  TResult,
-  TKey extends string,
-  TParent,
-  TContext,
-  TArgs,
-> =
+export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
   | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
   | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
 
-export type SubscriptionResolver<
-  TResult,
-  TKey extends string,
-  TParent = {},
-  TContext = {},
-  TArgs = {},
-> =
-  | ((
-      ...args: any[]
-    ) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
+  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
   | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
 
 export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
   parent: TParent,
   context: TContext,
-  info: GraphQLResolveInfo,
+  info: GraphQLResolveInfo
 ) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
 
-export type IsTypeOfResolverFn<T = {}, TContext = {}> = (
-  obj: T,
-  context: TContext,
-  info: GraphQLResolveInfo,
-) => boolean | Promise<boolean>;
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
 
 export type NextResolverFn<T> = () => Promise<T>;
 
-export type DirectiveResolverFn<
-  TResult = {},
-  TParent = {},
-  TContext = {},
-  TArgs = {},
-> = (
+export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
   next: NextResolverFn<TResult>,
   parent: TParent,
   args: TArgs,
   context: TContext,
-  info: GraphQLResolveInfo,
+  info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
 
 /** Mapping between all available schema types and the resolvers types */
@@ -293,10 +247,7 @@ export type ResolversParentTypes = {
   User: User;
 };
 
-export type DirectorResolvers<
-  ContextType = any,
-  ParentType extends ResolversParentTypes['Director'] = ResolversParentTypes['Director'],
-> = {
+export type DirectorResolvers<ContextType = any, ParentType extends ResolversParentTypes['Director'] = ResolversParentTypes['Director']> = {
   bio?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   birth?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   death?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
@@ -304,19 +255,13 @@ export type DirectorResolvers<
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
-export type GenreResolvers<
-  ContextType = any,
-  ParentType extends ResolversParentTypes['Genre'] = ResolversParentTypes['Genre'],
-> = {
+export type GenreResolvers<ContextType = any, ParentType extends ResolversParentTypes['Genre'] = ResolversParentTypes['Genre']> = {
   description?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
-export type MovieResolvers<
-  ContextType = any,
-  ParentType extends ResolversParentTypes['Movie'] = ResolversParentTypes['Movie'],
-> = {
+export type MovieResolvers<ContextType = any, ParentType extends ResolversParentTypes['Movie'] = ResolversParentTypes['Movie']> = {
   _id?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   description?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   director?: Resolver<ResolversTypes['Director'], ParentType, ContextType>;
@@ -328,113 +273,43 @@ export type MovieResolvers<
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
-export type QueryResolvers<
-  ContextType = any,
-  ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query'],
-> = {
-  discover?: Resolver<
-    Maybe<Array<Maybe<ResolversTypes['TMDBMovieSimple']>>>,
-    ParentType,
-    ContextType
-  >;
-  movie?: Resolver<
-    Maybe<ResolversTypes['TMDBMovieDetailed']>,
-    ParentType,
-    ContextType,
-    RequireFields<QueryMovieArgs, 'id'>
-  >;
-  movies?: Resolver<
-    Maybe<Array<Maybe<ResolversTypes['Movie']>>>,
-    ParentType,
-    ContextType
-  >;
-  users?: Resolver<
-    Maybe<Array<Maybe<ResolversTypes['User']>>>,
-    ParentType,
-    ContextType
-  >;
+export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
+  discover?: Resolver<Maybe<Array<Maybe<ResolversTypes['TMDBMovieSimple']>>>, ParentType, ContextType>;
+  movie?: Resolver<Maybe<ResolversTypes['TMDBMovieDetailed']>, ParentType, ContextType, RequireFields<QueryMovieArgs, 'id'>>;
+  movies?: Resolver<Maybe<Array<Maybe<ResolversTypes['Movie']>>>, ParentType, ContextType>;
+  users?: Resolver<Maybe<Array<Maybe<ResolversTypes['User']>>>, ParentType, ContextType>;
 };
 
-export type TmdbConfigurationResolvers<
-  ContextType = any,
-  ParentType extends ResolversParentTypes['TMDBConfiguration'] = ResolversParentTypes['TMDBConfiguration'],
-> = {
-  backdrop_sizes?: Resolver<
-    Array<ResolversTypes['String']>,
-    ParentType,
-    ContextType
-  >;
+export type TmdbConfigurationResolvers<ContextType = any, ParentType extends ResolversParentTypes['TMDBConfiguration'] = ResolversParentTypes['TMDBConfiguration']> = {
+  backdrop_sizes?: Resolver<Array<ResolversTypes['String']>, ParentType, ContextType>;
   base_url?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  logo_sizes?: Resolver<
-    Array<ResolversTypes['String']>,
-    ParentType,
-    ContextType
-  >;
-  poster_sizes?: Resolver<
-    Array<ResolversTypes['String']>,
-    ParentType,
-    ContextType
-  >;
-  profile_sizes?: Resolver<
-    Array<ResolversTypes['String']>,
-    ParentType,
-    ContextType
-  >;
+  logo_sizes?: Resolver<Array<ResolversTypes['String']>, ParentType, ContextType>;
+  poster_sizes?: Resolver<Array<ResolversTypes['String']>, ParentType, ContextType>;
+  profile_sizes?: Resolver<Array<ResolversTypes['String']>, ParentType, ContextType>;
   secure_base_url?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  still_sizes?: Resolver<
-    Array<ResolversTypes['String']>,
-    ParentType,
-    ContextType
-  >;
+  still_sizes?: Resolver<Array<ResolversTypes['String']>, ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
-export type TmdbMovieDetailedResolvers<
-  ContextType = any,
-  ParentType extends ResolversParentTypes['TMDBMovieDetailed'] = ResolversParentTypes['TMDBMovieDetailed'],
-> = {
+export type TmdbMovieDetailedResolvers<ContextType = any, ParentType extends ResolversParentTypes['TMDBMovieDetailed'] = ResolversParentTypes['TMDBMovieDetailed']> = {
   adult?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
-  backdrop_path?: Resolver<
-    Maybe<ResolversTypes['String']>,
-    ParentType,
-    ContextType
-  >;
+  backdrop_path?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
   budget?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
-  genre_ids?: Resolver<
-    Array<ResolversTypes['String']>,
-    ParentType,
-    ContextType
-  >;
+  genre_ids?: Resolver<Array<ResolversTypes['String']>, ParentType, ContextType>;
   homepage?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
   id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
   imdb_id?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
-  original_language?: Resolver<
-    ResolversTypes['Boolean'],
-    ParentType,
-    ContextType
-  >;
+  original_language?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
   original_title?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   overview?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
   popularity?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
   poster_path?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  production_companies?: Resolver<
-    Array<ResolversTypes['TMDBProductionCompany']>,
-    ParentType,
-    ContextType
-  >;
-  production_countries?: Resolver<
-    Array<ResolversTypes['TMDBProductionCountry']>,
-    ParentType,
-    ContextType
-  >;
+  production_companies?: Resolver<Array<ResolversTypes['TMDBProductionCompany']>, ParentType, ContextType>;
+  production_countries?: Resolver<Array<ResolversTypes['TMDBProductionCountry']>, ParentType, ContextType>;
   release_date?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   revenue?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
   runtime?: Resolver<Maybe<ResolversTypes['Int']>, ParentType, ContextType>;
-  spoken_languages?: Resolver<
-    Array<ResolversTypes['TMDBProductionSpokenLanguaeges']>,
-    ParentType,
-    ContextType
-  >;
+  spoken_languages?: Resolver<Array<ResolversTypes['TMDBProductionSpokenLanguaeges']>, ParentType, ContextType>;
   status?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   tagline?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
   title?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
@@ -444,23 +319,12 @@ export type TmdbMovieDetailedResolvers<
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
-export type TmdbMovieSimpleResolvers<
-  ContextType = any,
-  ParentType extends ResolversParentTypes['TMDBMovieSimple'] = ResolversParentTypes['TMDBMovieSimple'],
-> = {
+export type TmdbMovieSimpleResolvers<ContextType = any, ParentType extends ResolversParentTypes['TMDBMovieSimple'] = ResolversParentTypes['TMDBMovieSimple']> = {
   adult?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
   backdrop_path?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  genre_ids?: Resolver<
-    Array<ResolversTypes['String']>,
-    ParentType,
-    ContextType
-  >;
+  genre_ids?: Resolver<Array<ResolversTypes['String']>, ParentType, ContextType>;
   id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
-  original_language?: Resolver<
-    ResolversTypes['Boolean'],
-    ParentType,
-    ContextType
-  >;
+  original_language?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
   original_title?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   overview?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   popularity?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
@@ -473,51 +337,31 @@ export type TmdbMovieSimpleResolvers<
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
-export type TmdbProductionCompanyResolvers<
-  ContextType = any,
-  ParentType extends ResolversParentTypes['TMDBProductionCompany'] = ResolversParentTypes['TMDBProductionCompany'],
-> = {
+export type TmdbProductionCompanyResolvers<ContextType = any, ParentType extends ResolversParentTypes['TMDBProductionCompany'] = ResolversParentTypes['TMDBProductionCompany']> = {
   id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
-  logo_path?: Resolver<
-    Maybe<ResolversTypes['String']>,
-    ParentType,
-    ContextType
-  >;
+  logo_path?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
   name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   origin_country?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
-export type TmdbProductionCountryResolvers<
-  ContextType = any,
-  ParentType extends ResolversParentTypes['TMDBProductionCountry'] = ResolversParentTypes['TMDBProductionCountry'],
-> = {
+export type TmdbProductionCountryResolvers<ContextType = any, ParentType extends ResolversParentTypes['TMDBProductionCountry'] = ResolversParentTypes['TMDBProductionCountry']> = {
   iso_3166_1?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
-export type TmdbProductionSpokenLanguaegesResolvers<
-  ContextType = any,
-  ParentType extends ResolversParentTypes['TMDBProductionSpokenLanguaeges'] = ResolversParentTypes['TMDBProductionSpokenLanguaeges'],
-> = {
+export type TmdbProductionSpokenLanguaegesResolvers<ContextType = any, ParentType extends ResolversParentTypes['TMDBProductionSpokenLanguaeges'] = ResolversParentTypes['TMDBProductionSpokenLanguaeges']> = {
   iso_639_1?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
-export type UserResolvers<
-  ContextType = any,
-  ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User'],
-> = {
+export type UserResolvers<ContextType = any, ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']> = {
   _id?: Resolver<Maybe<ResolversTypes['ID']>, ParentType, ContextType>;
   birthday?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  favoriteMovies?: Resolver<
-    Array<Maybe<ResolversTypes['String']>>,
-    ParentType,
-    ContextType
-  >;
+  favoriteMovies?: Resolver<Array<Maybe<ResolversTypes['String']>>, ParentType, ContextType>;
   password?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   username?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
@@ -536,3 +380,4 @@ export type Resolvers<ContextType = any> = {
   TMDBProductionSpokenLanguaeges?: TmdbProductionSpokenLanguaegesResolvers<ContextType>;
   User?: UserResolvers<ContextType>;
 };
+
